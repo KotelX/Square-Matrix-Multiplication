43b2917c95cea898f234ad2ec3dda7ad5d28940a
Michail <73073160+KotelX@users.noreply.github.com>
2023-02-03 17:40:48 +0300

    Добавьте файлы проекта.

---
+++ Square Matrix Multiplication/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+public class Kata
+{
+    public static int[,] MatrixMultiplication(int[,] a, int[,] b)
+    {
+        var size = a.GetLength(0);
+        int[,] res = new int[size,size];
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                var summ = 0;
+                for (int k = 0; k < size; k++)
+                {
+                    summ += a[i, k] * b[k, j];
+                }
+                res[i, j] = summ;
+            }
+            
+        }
+
+        return res;
+    }
+}
+
